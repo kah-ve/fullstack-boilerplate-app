33b91ee5d4a37ab294d54fc8be98d4889f92bfe5
kah-ve <cam.abdurrahman@gmail.com>
2021-05-18 02:24:17 -0400

    updated frontpage to give feedback on which rows changed and which values changed.

--- client/src/components/FrontPage/FrontPage.js
+++ client/src/components/FrontPage/FrontPage.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import './FrontPage.css';
 
 async function sleep(ms) {
@@ -15,33 +15,30 @@ class FrontPage extends React.Component {
       temperature: 58,
       precipitation: 32,
       climate: 'The climate is quite the occassion!',
-      description: "A truly rainy day it is indeed."
+      description: "A truly rainy day it is indeed.",
+      prevRows: []
     }
 
     this.get_response = this.get_response.bind(this);
     this.post_to_db = this.post_to_db.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.parseOutput = this.parseOutput.bind(this);
-    this.getKey = this.getKey.bind(this);
     this.submitForm = this.submitForm.bind(this)
 
-    this.keyCount = 0;
-  }
-
-  getKey() {
-    return this.keyCount++;
   }
 
   handleChange(evt) {
     const value = evt.target.value;
-    console.log(`Value changing is ${value}`)
-    console.log(`Name that is changing ${evt.target.name}`)
+    // console.log(`Value changing is ${value}`)
+    // console.log(`Name that is changing ${evt.target.name}`)
     this.setState({
       [evt.target.name]: value
     });
   }
 
   async get_response() {
+    this.setState(() => ({ prevRows: this.state.response }));
+
     this.setState({
       response: 'Loading...'
     })
@@ -50,7 +47,7 @@ class FrontPage extends React.Component {
     try {
       const res = await fetch("http://localhost:5000/api/getinfo");
       const res_json = await res.json();
-      console.log(res_json)
+      // console.log(res_json)
       this.setState({
         response: res_json !== null && res_json[0] && res_json[0].length > 0 ? res_json : 'Nothing to Post'
       });
@@ -93,29 +90,14 @@ class FrontPage extends React.Component {
 
   parseOutput() {
     let value_arr = this.state.response;
+    let prevRows = this.state.prevRows;
 
     if (typeof value_arr === "string") {
       return value_arr
     }
 
-    const paddedString = (input) => {
-      let inputString = input.toString().trim();
-      let placeholderLength = 10;
-      if (placeholderLength < inputString.length)
-        return inputString;
-
-      return inputString + ' '.repeat(placeholderLength - inputString.length);
-    }
-
     return (
-      value_arr.map(element =>
-        <div className="outputTextFormat" key={this.getKey()}>
-          <pre className='identifier'>{paddedString(element[0])}</pre>
-          <pre className='identifier'>{paddedString(element[1])}</pre>
-          <pre className='identifier'>{paddedString(element[2])}</pre>
-          <pre className='identifier'>{paddedString(element[3])}</pre>
-          <pre className='identifier'>{paddedString(element[4])}</pre></div>
-      )
+      <DataRows rows={value_arr} prevRows={prevRows} />
     )
   }
 
@@ -167,6 +149,120 @@ class InputBox extends React.Component {
       </form>
     )
   }
+}
+
+
+function DataRows(props) {
+  let keyCount = 0
+
+  function findDifference(newRows, oldRows) {
+    let newlyChangedValues = new Map();
+
+    for (let i = 0; i < newRows.length; i++) {
+      let newDate = newRows[i][0]
+      let isNewDate = true;
+      for (let j = 0; j < oldRows.length; j++) {
+        let oldDate = oldRows[j][0]
+
+        if (newDate === oldDate) {
+          isNewDate = false;
+
+          // Also check to see if parameters have changed for that date
+          for (let k = 0; k < newRows[i].length; k++) {
+            if (newRows[i][k] !== oldRows[j][k]) {
+              if (newlyChangedValues[i]) {
+                console.log(newlyChangedValues[i])
+                newlyChangedValues[i] = newlyChangedValues[i].concat(k)
+              }
+              else {
+                newlyChangedValues[i] = [k]
+              }
+            }
+          }
+        }
+      }
+
+      if (isNewDate) {
+        // all the values have changed (since it has been added of course)
+        for (let h = 0; h < newRows[i].length; h++) {
+          if (newlyChangedValues[i]) {
+            console.log(newlyChangedValues[i])
+            newlyChangedValues[i] = newlyChangedValues[i].concat(h)
+          }
+          else {
+            newlyChangedValues[i] = [h]
+          }
+        }
+      }
+
+    }
+
+    console.log(`Newly Changed Values Size: ${newlyChangedValues.size}`)
+    console.log(`Newly Changed Values ${[...newlyChangedValues]}`);
+    return newlyChangedValues;
+
+    // for (let row of newRows) {
+    //   newDatesSet.add(row[0])
+    // }
+
+    // for (let row of oldRows) {
+    //   prevDatesSet.add(row[0])
+    // }
+
+    // let newlyAddedDatesSet = new Set([...newDatesSet].filter(x => !prevDatesSet.has(x)))
+    // return newlyAddedDatesSet
+  }
+
+
+  function outputDataRows(rows, oldRows) {
+    let newlyChangedValues = findDifference(rows, oldRows);
+    console.log(`New Dates: ${[...newlyChangedValues].map(element => console.log(element))}`)
+
+    let parsedRows = rows.map((element, index) => {
+      const numOfElements = 5
+      let orangeIfNew = []
+      for (let i = 0; i < numOfElements; i++) {
+        orangeIfNew.push('black');
+      }
+
+      if (newlyChangedValues[index]) {
+        for (let num of newlyChangedValues[index]) {
+          orangeIfNew[num] = 'orange';
+        }
+      }
+
+      return (
+        <div className="outputTextFormat" key={getKey()}>
+          <pre className='identifier' style={{ backgroundColor: orangeIfNew[0] }}>{paddedString(element[0])}</pre>
+          <pre className='identifier' style={{ backgroundColor: orangeIfNew[1] }}>{paddedString(element[1])}</pre>
+          <pre className='identifier' style={{ backgroundColor: orangeIfNew[2] }}>{paddedString(element[2])}</pre>
+          <pre className='identifier' style={{ backgroundColor: orangeIfNew[3] }}>{paddedString(element[3])}</pre>
+          <pre className='identifier' style={{ backgroundColor: orangeIfNew[4] }}>{paddedString(element[4])}</pre>
+        </div >
+      )
+    })
+
+    return parsedRows;
+  }
+
+  function paddedString(input) {
+    let inputString = input.toString().trim();
+    let placeholderLength = 10;
+    if (placeholderLength < inputString.length)
+      return inputString;
+
+    return inputString + ' '.repeat(placeholderLength - inputString.length);
+  }
+
+  function getKey() {
+    return keyCount++;
+  }
+
+  return (
+    <div>
+      { outputDataRows(props.rows, props.prevRows)}
+    </div>
+  )
 }
 
 export default FrontPage;
